cbe507a98fb96c956d80fefa3a5794b6f88edde6
Daniel Kliemsch <dk@lvl51.de>
2019-01-24 13:34:10 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# SilverStripe GridField button for excel exports
+
+Similar to the default GridFieldExportButton but exports Excel (xlsx) files instead of CSV. 
+
+## Maintainer
+- Daniel Kliemsch <dk@lvl51.de>
+- Julian Scheuchenzuber <js@lvl51.de>
---
+++ code/GridFieldExcelExportButton.php
@@ -0,0 +1,162 @@
+<?php
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
+class GridFieldExcelExportButton implements GridField_HTMLProvider, GridField_ActionProvider, GridField_URLHandler {
+
+    /**
+     * @var array Map of a property name on the exported objects, with values being the column title in the CSV file.
+     * Note that titles are only used when {@link $csvHasHeader} is set to TRUE.
+     */
+    protected $exportColumns;
+
+    protected $targetFragment;
+
+    public function __construct($targetFragment = 'before', $exportColumns = null) {
+        $this->targetFragment = $targetFragment;
+        $this->exportColumns = $exportColumns;
+    }
+
+    public function getHTMLFragments($gridField) {
+        $button = new GridField_FormAction(
+            $gridField,
+            'exportexcel',
+            _t(self::class . '.EXPORT_CTA'),
+            'exportexcel',
+            null
+        );
+        $button->setAttribute('data-icon', 'download-csv');
+        $button->addExtraClass('no-ajax action_export');
+        $button->setForm($gridField->getForm());
+
+        return [
+            $this->targetFragment => '<p class="grid-csv-button">' . $button->Field() . '</p>',
+        ];
+    }
+
+
+    public function getActions($gridField) {
+        return ['exportexcel'];
+    }
+
+
+    public function handleAction(GridField $gridField, $actionName, $arguments, $data) {
+        if ($actionName == 'exportexcel') {
+            return $this->handleExcelExport($gridField);
+        }
+    }
+
+    public function getURLHandlers($gridField) {
+        return [
+            'exportexcel' => 'handleExcelExport',
+        ];
+    }
+
+    protected function getExportColumnsForGridField(GridField $gridField) {
+        if ($this->exportColumns) {
+            $exportColumns = $this->exportColumns;
+        } else if ($dataCols = $gridField->getConfig()->getComponentByType('GridFieldDataColumns')) {
+            $exportColumns = $dataCols->getDisplayFields($gridField);
+        } else {
+            $exportColumns = singleton($gridField->getModelClass())->summaryFields();
+        }
+
+        return $exportColumns;
+    }
+
+    public function handleExcelExport($gridField, $request = null) {
+
+        // Setup filename and path (to tmp dir)
+        $now = Date("d-m-Y-H-i");
+        $fileName = "excel-export-$now.xlsx";
+        $path = Controller::join_links(sys_get_temp_dir(), $fileName);
+
+        // Setup spreadsheet
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+
+        $columns = $this->getExportColumnsForGridField($gridField);
+
+        // Get headers (@see GridFieldExportButton)
+        $headers = [];
+        foreach ($columns as $columnSource => $columnHeader) {
+            if (is_array($columnHeader) && array_key_exists('title', $columnHeader)) {
+                $headers[] = $columnHeader['title'];
+            } else {
+                $headers[] = (!is_string($columnHeader) && is_callable($columnHeader)) ? $columnSource : $columnHeader;
+            }
+        }
+
+        // Write headers
+        $sheet->fromArray($headers, null, 'A1');
+
+        // Collect items / content rows (@see GridFieldExportButton)
+        $items = $gridField->getManipulatedList();
+
+        // @todo should GridFieldComponents change behaviour based on whether others are available in the config?
+        foreach ($gridField->getConfig()->getComponents() as $component) {
+            if ($component instanceof GridFieldFilterHeader || $component instanceof GridFieldSortableHeader) {
+                $items = $component->getManipulatedData($gridField, $items);
+            }
+        }
+
+        $content = [];
+        foreach ($items->limit(null) as $item) {
+            if (!$item->hasMethod('canView') || $item->canView()) {
+                $columnData = array();
+
+                foreach ($columns as $columnSource => $columnHeader) {
+                    if (!is_string($columnHeader) && is_callable($columnHeader)) {
+                        if ($item->hasMethod($columnSource)) {
+                            $relObj = $item->{$columnSource}();
+                        } else {
+                            $relObj = $item->relObject($columnSource);
+                        }
+
+                        $value = $columnHeader($relObj);
+                    } else {
+                        $value = $gridField->getDataFieldValue($item, $columnSource);
+
+                        if ($value === null) {
+                            $value = $gridField->getDataFieldValue($item, $columnHeader);
+                        }
+                    }
+
+                    $value = str_replace(array("\r", "\n"), "\n", $value);
+
+                    // [SS-2017-007] Sanitise XLS executable column values with a leading tab
+                    if (!Config::inst()->get(get_class($this), 'xls_export_disabled')
+                        && preg_match('/^[-@=+].*/', $value)
+                    ) {
+                        $value = "\t" . $value;
+                    }
+                    $columnData[] = $value;
+                }
+
+                $content[] = $columnData;
+            }
+
+            if ($item->hasMethod('destroy')) {
+                $item->destroy();
+            }
+        }
+
+        // Write content rows
+        $sheet->fromArray($content, null, 'A2');
+
+        // Auto size column width
+        // TODO prepare for larger ranges
+        foreach (range('A', range('A', 'Z')[count($headers)]) as $columnID) {
+            $sheet->getColumnDimension($columnID)
+                ->setAutoSize(true);
+        }
+
+        // Write xlsx file
+        $writer = new Xlsx($spreadsheet);
+        $writer->save($path);
+
+        // Read and return file content (triggers download) (MIME Type: https://blogs.msdn.microsoft.com/vsofficedeveloper/2008/05/08/office-2007-file-format-mime-types-for-http-content-streaming-2/)
+        return SS_HTTPRequest::send_file(file_get_contents($path), $fileName, 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+    }
+}
